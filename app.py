7e96360db1083fcd7b5b32dab64567dcc205cecf
FirstPMT <smokezkiller@gmail.com>
2025-12-01 15:24:42 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+
+st.button("Rerun")
+# The title of your web application
+st.title('My First Streamlit App')
+# A header for a section
+st.header('Hello, Streamlit World!')
+# The 'magic' write command can handle almost anything
+st.write('This web app is running entirely from Python.')
+# Display text formatted with Markdown
+st.markdown('### Streamlit makes development **easy** and')             
+
+
+
+
