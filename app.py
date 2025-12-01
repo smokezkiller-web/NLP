72cf03c9114436cb5bc8cca598064ef078749e18
FirstPMT <smokezkiller@gmail.com>
2025-12-01 23:03:05 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-
+import google.generativeai as genai
+import os
+import pandas as pd
+import json
 import streamlit as st
+
+api_key = st.text_input("Input your Gemini api key here")
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel("gemini-2.5-flash-lite")
+
+
+
+
+
+git config --global user.email "smokezkiller@gmail.com"
+git config --global user.name "smokezkiller-web"
+
 
 st.button("Rerun")
 # The title of your web application
@@ -7,7 +22,7 @@ st.title('My First Streamlit App')
 # A header for a section
 st.header('Hello, Streamlit World!')
 # The 'magic' write command can handle almost anything
-st.write('This web app is running entirely from Python.')
+st.write(" ")
 # Display text formatted with Markdown
 st.markdown('### Streamlit makes development **easy** and')             
 
